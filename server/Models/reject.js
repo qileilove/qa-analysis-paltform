afc9080fa5bb09c03ae1ea8ca32c491d7d3220db
Lei Qi <qileilove@gmail.com>
2017-05-22 10:39:32 +0800

    [QL] add qa_reject_cost_times and one_pass filed into  reject collection

--- server/Models/reject.js
+++ server/Models/reject.js
@@ -6,8 +6,10 @@ const RejectSchema = new mongoose.Schema({
     reject_story_id:String,
     iteration_number: String,
     story_name: String,
-    reject_cost_time: String,
+    lt_reject_cost_times: String,
+    qa_reject_cost_times:String,
     fix_cost_time: String,
+    one_pass:String,
     reject_reason: String
 });
 
--- server/api/addReject.js
+++ server/api/addReject.js
@@ -3,24 +3,27 @@ import Reject from '../Models/reject';
 import qs from 'qs';
 
 export default function(req,res,next){
-    const reject_cost_time = qs.parse(req.body).reject_cost_time;
+    const lt_reject_cost_times = qs.parse(req.body).lt_reject_cost_times;
     const iteration_number = qs.parse(req.body).iteration_number;
     const reject_story_id = qs.parse(req.body).reject_story_id;
     const story_name = qs.parse(req.body).story_name;
     const fix_cost_time = qs.parse(req.body).fix_cost_time;
     const reject_reason = qs.parse(req.body).reject_reason;
+    const one_pass = qs.parse(req.body).one_pass;
+    const qa_reject_cost_times = qs.parse(req.body).qa_reject_cost_times;
 
     const post = {
         reject_story_id: reject_story_id,
         iteration_number: iteration_number,
         story_name: story_name,
-        reject_cost_time:reject_cost_time,
+        lt_reject_cost_times:lt_reject_cost_times,
         fix_cost_time:fix_cost_time,
+        one_pass:one_pass,
+        qa_reject_cost_times:qa_reject_cost_times,
         reject_reason:reject_reason
 
     }
     let rejectEntity = new Reject(post);
-    console.log("asdsadsadasddsaasddas");
     rejectEntity.save((err,doc)=>{
       if(err){
             return res.status(500).end('create failed')
