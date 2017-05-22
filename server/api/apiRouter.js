7e1ddcb1d5e705332e9fb4d8a93852159d0f19e0
Lei Qi <qileilove@gmail.com>
2017-05-22 13:49:37 +0800

    [QL] remove useless js file

--- server/api/apiRouter.js
+++ server/api/apiRouter.js
@@ -1,47 +1,15 @@
 import Express from 'express';
-import getItem from './getItem';
-import getList from './getList';
-import user from './user';
-import addPost from './addPost';
+
 import getReject from './getReject';
+
 import editReject from './editReject';
 
-import log from './log';
-import reg from './reg';
-import comment from './comment';
-import vote from './vote';
-import auth from '../middleware/auth';
-import sortUsers from './sortUsers';
-import getUserInfo from './getUserInfo';
 import addReject from './addReject';
 
 const router = Express.Router();
 
-router.get('/post',getList);
-
-router.post('/post',auth);
-
-router.post('/post',addPost);
-
 router.post('/reject',addReject);
-
-router.get('/detail',getItem);
-
-router.post('/log',log);
-
-router.post('/reg',reg);
-
-router.post('/user',user);
 router.get('/getReject',getReject);
 router.put('/editReject',editReject);
 
-router.post('/comment',auth);
-router.post('/comment',comment)
-
-router.post('/vote',auth);
-router.post('/vote',vote);
-
-router.get('/sortUsers',sortUsers);
-
-router.post('/getUserInfo',getUserInfo)
 export default router;
