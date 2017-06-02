cb0b9300852965d0a3de4aaae5cc84855d4e9ffa
Lei Qi <qileilove@gmail.com>
2017-06-02 10:37:50 +0800

    [QL] refactor the rest api for get reject collection

--- server/api/getReject.js
+++ server/api/getReject.js
@@ -1,20 +1,28 @@
 const jwt = require("jwt-simple");
 import Reject from '../Models/reject';
+import qs from 'qs';
+
 // const rejectEntity = new Reject();
     // let name = req.reject_story_id;
 
 export default function(req,res,next){
- if(req.params.reject_story_id == undefined){
+//get all data
+ if(qs.parse(req.query).reject_story_id == undefined){
+
     Reject.find({
     },(err,result)=>{
         if(err){
             return res.status(500).end('服务器错误');
         }
+
         return res.status(200).json(result);
     })
  }
+ // get data by reject_story_id
  else{
-     Reject.findOne({"reject_story_id": req.params.reject_story_id}, function(err, result) {
+   let query_story_id ={ reject_story_id:qs.parse(req.query).reject_story_id};
+
+     Reject.findOne(query_story_id, function(err, result) {
        if (err){ return handleError(err);}
 
      return res.status(200).json(result);
